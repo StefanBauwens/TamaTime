0f7096ad17b0fa4268073b40f68b6562d4273b25
Stefan Bauwens <stefan.bauwens@hotmail.com>
2026-04-18 10:40:10 +0800

    show actual time

--- TamagotchiFace/src/c/tamagotchi_face.c
+++ TamagotchiFace/src/c/tamagotchi_face.c
@@ -471,6 +471,40 @@ static void DrawBitmap(bool (*screen)[LCD_WIDTH],
     }
 }
 
+static void DrawBigDigit(bool (*screen)[LCD_WIDTH], int digit, uint8_t start_x, uint8_t start_y)
+{
+  switch (digit) {
+    case 0: DrawBitmap(screen, big_0, 4, 7, start_x, start_y); break;
+    case 1: DrawBitmap(screen, big_1, 4, 7, start_x, start_y); break;
+    case 2: DrawBitmap(screen, big_2, 4, 7, start_x, start_y); break;
+    case 3: DrawBitmap(screen, big_3, 4, 7, start_x, start_y); break;
+    case 4: DrawBitmap(screen, big_4, 4, 7, start_x, start_y); break;
+    case 5: DrawBitmap(screen, big_5, 4, 7, start_x, start_y); break;
+    case 6: DrawBitmap(screen, big_6, 4, 7, start_x, start_y); break;
+    case 7: DrawBitmap(screen, big_7, 4, 7, start_x, start_y); break;
+    case 8: DrawBitmap(screen, big_8, 4, 7, start_x, start_y); break;
+    case 9: DrawBitmap(screen, big_9, 4, 7, start_x, start_y); break;
+    default: break;
+  }
+}
+
+static void DrawSmallDigit(bool (*screen)[LCD_WIDTH], int digit, uint8_t start_x, uint8_t start_y)
+{
+  switch (digit) {
+    case 0: DrawBitmap(screen, small_0, 3, 5, start_x, start_y); break;
+    case 1: DrawBitmap(screen, small_1, 3, 5, start_x, start_y); break;
+    case 2: DrawBitmap(screen, small_2, 3, 5, start_x, start_y); break;
+    case 3: DrawBitmap(screen, small_3, 3, 5, start_x, start_y); break;
+    case 4: DrawBitmap(screen, small_4, 3, 5, start_x, start_y); break;
+    case 5: DrawBitmap(screen, small_5, 3, 5, start_x, start_y); break;
+    case 6: DrawBitmap(screen, small_6, 3, 5, start_x, start_y); break;
+    case 7: DrawBitmap(screen, small_7, 3, 5, start_x, start_y); break;
+    case 8: DrawBitmap(screen, small_8, 3, 5, start_x, start_y); break;
+    case 9: DrawBitmap(screen, small_9, 3, 5, start_x, start_y); break;
+    default: break;
+  }
+}
+
 static void update_time()
 {
   // clear screen used for time
@@ -486,26 +520,57 @@ static void update_time()
   struct tm *tick_time = localtime(&temp);
 
   //TODO add if statement for which screen to use
-  // time
-  DrawBitmap(s_screen_buffer, big_1, 4, 7, 2, 0); // hour position 1 //TODO handle hour correctly! take 24h into account
-  DrawBitmap(s_screen_buffer, big_5, 4, 7, 7, 0); // hour position 2
-  DrawBitmap(s_screen_buffer, big_3, 4, 7, 14, 0); // minute position 1
-  DrawBitmap(s_screen_buffer, big_6, 4, 7, 19, 0); // minute position 2
-  DrawBitmap(s_screen_buffer, small_0, 3, 5, 25, 2); // second position 1
-  DrawBitmap(s_screen_buffer, small_5, 3, 5, 29, 2); // second position 2
-
-  // am/pm 
-  if (!clock_is_24h_style())
+  // get correct hour for am or pm or 24h style
+  int hour = 0;
+  int minute = tick_time->tm_min;
+  int second = tick_time->tm_sec;
+  if (clock_is_24h_style())
   {
-    if (tick_time->tm_hour >= 13)
+    hour = tick_time->tm_hour;
+  }
+  else
+  {
+    hour = tick_time->tm_hour;
+    if (hour >= 13)
     {
+      hour -= 12;
+      // show pm
       DrawBitmap(s_screen_buffer, pm, 6, 8, 3, 8);
     }
     else
     {
+      // show am
       DrawBitmap(s_screen_buffer, am, 6, 8, 3, 8);
     }
   }
+
+  // handle first digit of the hour
+  int hour1 = 0;
+  if (hour >= 10)
+  {
+    hour1 = hour/10;
+    DrawBigDigit(s_screen_buffer, hour1, 2, 0); // only draw first digit if there is one (no zero)
+  }
+  // 2nd digit of the hour
+  DrawBigDigit(s_screen_buffer, hour - (hour1 * 10), 7, 0);
+  // 1st digit minute
+  int minute1 = 0;
+  if (minute >= 10)
+  {
+    minute1 = minute/10;
+  } 
+  DrawBigDigit(s_screen_buffer, minute1, 14, 0);
+  // 2nd digit minute
+  DrawBigDigit(s_screen_buffer, minute - (minute1 * 10), 19, 0);
+  // 1st digit second //TODO only show seconds if doing seconds
+  int second1 = 0;
+  if (second >= 10)
+  {
+    second1 = second/10;
+  }
+  DrawSmallDigit(s_screen_buffer, second1, 25, 2);
+  // 2nd digit second
+  DrawSmallDigit(s_screen_buffer, second - (second1 * 10), 29, 2);
 
   // arrows //TODO don't draw if no seconds? or just keep it fixed
   DrawBitmap(s_screen_buffer, arrow_full, 3, 5, 17, 10);
